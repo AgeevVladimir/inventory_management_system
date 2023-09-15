008cf242a0a8e04ddbb70e4acd8b8f04d0b475d7
Vladimir Ageev <ageev.v.s@gmail.com>
2023-09-16 00:07:09 +0200

    pytests added

---
+++ tests/test_inventory.py
@@ -0,0 +1,53 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from inventory import Inventory
+from product import Product, ElectronicProduct
+from serializer import serialize_inventory, deserialize_inventory
+
+
+def test_add_product():
+    inventory = Inventory()
+    product = Product(name="TestProduct", price=100.0, quantity=5)
+
+    inventory.add_product(product)
+
+    assert inventory.check_quantity("TestProduct") == 5
+
+
+def test_remove_product():
+    inventory = Inventory()
+    product = Product(name="TestProduct", price=100.0, quantity=5)
+    inventory.add_product(product)
+
+    inventory.remove_product("TestProduct")
+
+    assert inventory.check_quantity("TestProduct") == 0
+
+
+def test_update_product():
+    inventory = Inventory()
+    product = ElectronicProduct(name="Laptop", price=1000, quantity=3, brand="BrandX", model="ModelY",
+                                warranty_period=12)
+    inventory.add_product(product)
+
+    inventory.update_product("Laptop", {"price": 900})
+
+    updated_product = inventory.products["Laptop"]
+    assert updated_product.price == 900
+
+
+def test_serialize_deserialize():
+    inventory = Inventory()
+    product = ElectronicProduct(name="Laptop", price=1000, quantity=3, brand="BrandX", model="ModelY",
+                                warranty_period=12)
+    inventory.add_product(product)
+
+    filename = "test_inventory_data.json"
+    serialize_inventory(inventory, filename)
+    new_inventory = deserialize_inventory(filename)
+
+    assert new_inventory.check_quantity("Laptop") == 3
+    assert new_inventory.products["Laptop"].brand == "BrandX"
